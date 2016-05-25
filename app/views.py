a34628cca68c3a218fb6a07749156fbc31bf32b8
Pontus Wiberg <pontus@pontusw.com>
2016-05-25 11:30:39 +0200

    more updates

--- app/views.py
+++ app/views.py
@@ -12,8 +12,6 @@ from config import admin, WTF_CSRF_ENABLED, SECRET_KEY
 SCOPES = ['https://www.googleapis.com/auth/admin.directory.user', 'https://www.googleapis.com/auth/admin.directory.group', 'https://www.googleapis.com/auth/drive']
 APPLICATION_NAME = 'Google Drive Sharing Admin'
 
-pageTokens = []
-
 @app.route('/')
 def index():
 	searchform = SearchUserForm()
@@ -21,51 +19,22 @@ def index():
 	return render_template('index.html',
 							searchform=searchform)
 
-@app.route('/items/getnext/<user>/<prevtoken>')
-def getNextItems(user, prevtoken):
+@app.route('/items/get/<user>')
+def getItems(user):
 	if 'page' in request.args:
-		page = request.args['page']
-	searchform = SearchUserForm()
-	credentials = authenticate(user)
-	http_auth = credentials.authorize(httplib2.Http())
-	url_get = 'https://www.googleapis.com/drive/v2/files?'
-	query = urllib.urlencode({'q': '\'' + user + '\' in owners', 'pageToken': prevtoken})
-	if prevtoken not in pageTokens:
-		pageTokens.append(prevtoken)
-	url_get += query
-	content = http_auth.request(url_get, "GET")
-	info = json.loads(content[1])
-	itemarray = []
-	if 'nextPageToken' in info:
-		token = info['nextPageToken']
-		print token
-	else:
-		token = False
-	infolist = info['items']
-	for item in infolist:
-		owners = item['owners']
-		ownersarr = []
-		for owner in owners:
-			ownersarr.append(owner['emailAddress'])
-		itemobj = { 'title': item['title'], 'id': item['id'], 'shared': item['shared'], 'owners': ownersarr }
-		itemarray.append(itemobj)
-	#url_get = 'https://www.googleapis.com/drive/v2/files?pageToken=' + token
-	#content = http_auth.request(url_get, "GET")
-	#info = json.loads(content[1])
-	return render_template('items.html',
-							searchform=searchform, 
-							items=itemarray,
-							previouspage=prevpage,
-							ref=page,
-							nextpage=token,
-							user=user)
-
-@app.route('/items/getall/<user>')
-def getAllItems(user):
-	if 'page' in request.args:
-		page = int(request.args['page'])
+		if request.args['page'] == 'all':
+			page = 'all'
+		else:
+			page = int(request.args['page'])
 	else:
 		page = 0
+	if 'shared' in request.args:
+		if request.args['shared'] == 'True':
+			shared = True
+		elif request.args['shared'] == 'False':
+			shared = 'no'
+	else:
+		shared = False
 	searchform = SearchUserForm()
 	credentials = authenticate(user)
 	http_auth = credentials.authorize(httplib2.Http())
@@ -74,6 +43,10 @@ def getAllItems(user):
 		session['pageTokens'] = ['']
 		query = urllib.urlencode({'q': '\'' + user + '\' in owners'})
 		previouspage = False
+	elif page == 'all':
+		query = urllib.urlencode({'q': '\'' + user + '\' in owners'})
+		previouspage = False
+		nextpage = False
 	else:
 		useToken = session['pageTokens'][page]
 		query = urllib.urlencode({'q': '\'' + user + '\' in owners', 'pageToken': useToken})
@@ -82,86 +55,119 @@ def getAllItems(user):
 	content = http_auth.request(url_get, "GET")
 	info = json.loads(content[1])
 	itemarray = []
-	if 'nextPageToken' in info:
-		nextpage = True
-		token = info['nextPageToken']
-		if token not in pageTokens:
-			session['pageTokens'].insert(page+1, token)
+	if page == 'all':
+		while 'nextPageToken' in info:
+			infolist = info['items']
+			for item in infolist:
+				if shared == True:
+					if item['shared'] == True:
+						owners = item['owners']
+						ownersarr = []
+						for owner in owners:
+							ownersarr.append(owner['emailAddress'])
+						itemobj = { 'title': item['title'], 'id': item['id'], 'shared': item['shared'], 'owners': ownersarr }
+						itemarray.append(itemobj)
+				elif shared == 'no':
+					if item['shared'] == False:
+						owners = item['owners']
+						ownersarr = []
+						for owner in owners:
+							ownersarr.append(owner['emailAddress'])
+						itemobj = { 'title': item['title'], 'id': item['id'], 'shared': item['shared'], 'owners': ownersarr }
+						itemarray.append(itemobj)
+				else:
+					owners = item['owners']
+					ownersarr = []
+					for owner in owners:
+						ownersarr.append(owner['emailAddress'])
+					itemobj = { 'title': item['title'], 'id': item['id'], 'shared': item['shared'], 'owners': ownersarr }
+					itemarray.append(itemobj)
+			url_get = 'https://www.googleapis.com/drive/v2/files?'
+			query = urllib.urlencode({'q': '\'' + user + '\' in owners', 'pageToken': info['nextPageToken']})
+			url_get += query
+			content = http_auth.request(url_get, "GET")
+			info = json.loads(content[1])
 	else:
-		nextpage = False
-	infolist = info['items']
-	for item in infolist:
-		owners = item['owners']
-		ownersarr = []
-		for owner in owners:
-			ownersarr.append(owner['emailAddress'])
-		itemobj = { 'title': item['title'], 'id': item['id'], 'shared': item['shared'], 'owners': ownersarr }
-		itemarray.append(itemobj)
-	#url_get = 'https://www.googleapis.com/drive/v2/files?pageToken=' + token
-	#content = http_auth.request(url_get, "GET")
-	#info = json.loads(content[1])
+		if 'nextPageToken' in info:
+			nextpage = True
+			token = info['nextPageToken']
+			if token not in session['pageTokens']:
+				session['pageTokens'].insert(page+1, token)
+		else:
+			nextpage = False
+		infolist = info['items']
+		for item in infolist:
+			if shared == True:
+				if item['shared'] == True:
+					owners = item['owners']
+					ownersarr = []
+					for owner in owners:
+						ownersarr.append(owner['emailAddress'])
+					itemobj = { 'title': item['title'], 'id': item['id'], 'shared': item['shared'], 'owners': ownersarr }
+					itemarray.append(itemobj)
+			elif shared == 'no':
+				if item['shared'] == False:
+					owners = item['owners']
+					ownersarr = []
+					for owner in owners:
+						ownersarr.append(owner['emailAddress'])
+					itemobj = { 'title': item['title'], 'id': item['id'], 'shared': item['shared'], 'owners': ownersarr }
+					itemarray.append(itemobj)
+			else:
+				owners = item['owners']
+				ownersarr = []
+				for owner in owners:
+					ownersarr.append(owner['emailAddress'])
+				itemobj = { 'title': item['title'], 'id': item['id'], 'shared': item['shared'], 'owners': ownersarr }
+				itemarray.append(itemobj)
 	return render_template('items.html',
 							searchform=searchform, 
 							items=itemarray,
 							nextpage=nextpage,
 							previouspage=previouspage,
 							user=user,
-							page=page)
+							page=page,
+							shared=shared)
 
-@app.route('/items/getall/full/<user>')
-def getAllItemsFull(user):
+
+@app.route('/item/get/<user>/<item>')
+def getItem(user, item):
+	if 'title' in request.args:
+		title = request.args['title']
+	else:
+		title = ''
 	searchform = SearchUserForm()
 	credentials = authenticate(user)
 	http_auth = credentials.authorize(httplib2.Http())
-	url_get = 'https://www.googleapis.com/drive/v2/files?'
-	query = urllib.urlencode({'q': '\'' + user + '\' in owners'})
-	url_get += query
+	url_get = 'https://www.googleapis.com/drive/v2/files/' + item + '/permissions'
 	content = http_auth.request(url_get, "GET")
 	info = json.loads(content[1])
-	itemarray = []
-	while 'nextPageToken' in info:
-		token = info['nextPageToken']
-		infolist = info['items']
-		for item in infolist:
-			owners = item['owners']
-			ownersarr = []
-			for owner in owners:
-				ownersarr.append(owner['emailAddress'])
-			itemobj = { 'title': item['title'], 'id': item['id'], 'shared': item['shared'], 'owners': ownersarr }
-			itemarray.append(itemobj)
-		url_get = 'https://www.googleapis.com/drive/v2/files?'
-		query = urllib.urlencode({'q': '\'' + user + '\' in owners', 'pageToken': token})
-		url_get += query
-		content = http_auth.request(url_get, "GET")
-		info = json.loads(content[1])
-	return render_template('items.html',
-							searchform=searchform, 
-							items=itemarray,
+	permissionsobj = {'owners': [], 'writers': [], 'readers': []}
+	permissions = info['items']
+	for permission in permissions:
+		print(permissionsobj)
+		name = permission['name']
+		role = permission['role']
+		if permission['type'] == 'user':
+			mail = permission['emailAddress']
+		elif permission['type'] == 'domain':
+			mail = 'domain'
+		if role == 'owner':
+			permissionsobj['owners'].append({'name': name, 'mail': mail})
+		if role == 'writer':
+			permissionsobj['writers'].append({'name': name, 'mail': mail})
+		if role == 'reader':
+			permissionsobj['readers'].append({'name': name, 'mail': mail})
+	#for owner in owners:
+	#	ownersarr.append(owner['emailAddress'])
+	#itemobj = { 'title': item['title'], 'id': item['id'], 'shared': item['shared'], 'owners': ownersarr }
+	#itemarray.append(itemobj)
+	return render_template('item.html',
+							searchform=searchform,
+							title=title,
+							item=info,
+							permissions=permissionsobj,
 							user=user)
-
-@app.route('/items/getshared/<user>')
-def getSharedItems(user):
-	searchform = SearchUserForm()
-	credentials = authenticate(user)
-	http_auth = credentials.authorize(httplib2.Http())
-	url_get = 'https://www.googleapis.com/drive/v2/files'
-	content = http_auth.request(url_get, "GET")
-	info = json.loads(content[1])
-	itemarray = []
-	while 'nextPageToken' in info:
-		token = info['nextPageToken']
-		print token
-		infolist = info['items']
-		for item in infolist:
-			if item['shared'] == True:
-				itemobj = { 'title': item['title'], 'id': item['id'], 'shared': item['shared'], 'owners': item['ownerNames'] }
-				itemarray.append(itemobj)
-		url_get = 'https://www.googleapis.com/drive/v2/files?pageToken=' + token
-		content = http_auth.request(url_get, "GET")
-		info = json.loads(content[1])
-	return render_template('items.html',
-							searchform=searchform, 
-							items=itemarray)
 
 @app.route('/users/get/<user>')
 def getUser(user):
@@ -171,7 +177,7 @@ def getUser(user):
 							user=user)
 
 @app.route('/users/list')
-def userList():
+def listUsers():
 	searchform = SearchUserForm()
 	credentials = authenticate(admin)
 	http_auth = credentials.authorize(httplib2.Http())
